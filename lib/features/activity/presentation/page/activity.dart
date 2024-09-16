969367847a8bcb1d349fb721e40ee7cc5f02f71e
hanseehhh <mrhns00@gmail.com>
2024-09-16 18:35:47 +0700

    update activity

--- lib/features/activity/presentation/page/activity.dart
+++ lib/features/activity/presentation/page/activity.dart
@@ -40,7 +40,7 @@ class WhiteScreen extends StatelessWidget {
                   child: Padding(
                     padding: EdgeInsets.only(left: 16),
                     child: Text(
-                      'Kumpulan Promo Yang Ada',
+                      'Promos',
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 16,
@@ -59,7 +59,7 @@ class WhiteScreen extends StatelessWidget {
             const SizedBox(height: 20),
             _buildHistoryCard(
               location:
-                  'Bandung, Indonesia Indonesia Indonesia Indonesia Indonesia Indonesia',
+                  'Bandung, Indonesia',
               price: 'Rp 75.000',
             ),
             const SizedBox(height: 20),
