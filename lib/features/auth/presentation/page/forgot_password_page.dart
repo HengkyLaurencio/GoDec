521468f8c9a67114f2311f79ced7b5c1e41aca68
BoviliusMeidi <boviliusbm@gmail.com>
2024-09-17 13:25:06 +0700

    change : unite with firebase

--- lib/features/auth/presentation/page/forgot_password_page.dart
+++ lib/features/auth/presentation/page/forgot_password_page.dart
@@ -1,11 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:godec/features/auth/presentation/widget/auth_notif.dart';
 
 import '../widget/auth_bottom.dart';
 import '../widget/auth_field.dart';
 
-class ForgotPasswordPage extends StatelessWidget {
+class ForgotPasswordPage extends StatefulWidget {
   const ForgotPasswordPage({super.key});
+
+  @override
+  State<ForgotPasswordPage> createState() => _ForgotPasswordPageState();
+}
+
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final emailController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  String? _errorMsg;
 
   @override
   Widget build(BuildContext context) {
@@ -35,13 +46,44 @@ class ForgotPasswordPage extends StatelessWidget {
                       const Text('Forgot Password',
                           style: TextStyle(fontSize: 24)),
                       const SizedBox(height: 60.0),
-                      const AuthField(
-                          label: 'Enter Your Email',
-                          type: TextInputType.emailAddress,
-                          password: false),
+                      Form(
+                        key: _formKey,
+                        child: AuthField(
+                          controller: emailController,
+                          hintText: 'Email',
+                          obscureText: false,
+                          keyboardType: TextInputType.emailAddress,
+                          prefixIcon: const Icon(Icons.mail),
+                          errorMsg: _errorMsg,
+                          validator: (val) {
+                            if (val!.isEmpty) {
+                              return 'Please fill in this field';
+                            } else if (!RegExp(
+                                    r'^[\w-\.]+@([\w-]+.)+[\w-]{2,4}$')
+                                .hasMatch(val)) {
+                              return 'Please enter a valid email';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
                       const SizedBox(height: 20.0),
                       ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            try {
+                              await FirebaseAuth.instance
+                                  .sendPasswordResetEmail(
+                                      email: emailController.text);
+                            // ignore: use_build_context_synchronously
+                            AuthNotif.show(context, 'Password reset email sent');
+                            } catch (error) {
+                              setState(() {
+                                _errorMsg = 'Error: ${error.toString()}';
+                              });
+                            }
+                          }
+                        },
                         style: ElevatedButton.styleFrom(
                           minimumSize: const Size(200, 50),
                           backgroundColor:
--- lib/features/auth/presentation/page/signup_page.dart
+++ lib/features/auth/presentation/page/signup_page.dart
@@ -1,112 +1,253 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../blocs/sign_up_bloc/sign_up_bloc.dart';
+import '../../data/models/user.dart';
 import '../widget/auth_bottom.dart';
 import '../widget/auth_field.dart';
 
-class SignupPage extends StatelessWidget {
+class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final repasswordController = TextEditingController();
+  final phoneNumberController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  bool obscurePassword = true;
+  bool signUpRequired = false;
+  String? _errorMsg;
 
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
         const SystemUiOverlayStyle(statusBarColor: Colors.black));
 
-    return Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        body: Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/BG_Auth_2.png'),
-                  fit: BoxFit.cover)),
-          child: SingleChildScrollView(
-            child: SafeArea(
-              child: Container(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    const SizedBox(height: 20.0),
-                    Text('Sign Up',
-                        style: TextStyle(
-                            fontSize: 24,
-                            color: Theme.of(context).colorScheme.surface)),
-                    const SizedBox(height: 100.0),
-                    const AuthField(
-                        label: 'Name',
-                        type: TextInputType.name,
-                        password: false),
-                    const SizedBox(height: 10.0),
-                    const AuthField(
-                        label: 'Email',
-                        type: TextInputType.emailAddress,
-                        password: false),
-                    const SizedBox(height: 10.0),
-                    const AuthField(
-                        label: 'Phone Number',
-                        type: TextInputType.number,
-                        password: false),
-                    const SizedBox(height: 10.0),
-                    const AuthField(
-                        label: 'Password',
-                        type: TextInputType.text,
-                        password: true),
-                    const SizedBox(height: 10.0),
-                    const AuthField(
-                        label: 'Re-Enter Password',
-                        type: TextInputType.emailAddress,
-                        password: true),
-                    const SizedBox(height: 10.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return BlocListener<SignUpBloc, SignUpState>(
+      listener: (context, state) {
+        if (state is SignUpSuccess) {
+          setState(() {
+            context.go('/auth/login');
+          });
+        } else if (state is SignUpProcess) {
+          setState(() {
+            signUpRequired = true;
+          });
+        } else if (state is SignUpFailure) {
+          setState(() {
+            signUpRequired = false;
+            _errorMsg = 'Invalid sign up';
+          });
+        }
+      },
+      child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          body: Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/BG_Auth_2.png'),
+                    fit: BoxFit.cover)),
+            child: SingleChildScrollView(
+              child: SafeArea(
+                child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        Expanded(
-                          child: Row(
-                            children: [
-                              Checkbox(
-                                value: false,
-                                onChanged: (bool? value) {},
-                              ),
-                              const SizedBox(width: 8.0),
-                              const Flexible(
-                                child: Text(
-                                  'I have read and agree to the terms of service and privacy policy',
-                                  overflow: TextOverflow.visible,
-                                ),
-                              ),
-                            ],
-                          ),
+                        const SizedBox(height: 20.0),
+                        Text('Sign Up',
+                            style: TextStyle(
+                                fontSize: 24,
+                                color: Theme.of(context).colorScheme.surface)),
+                        const SizedBox(height: 80.0),
+                        AuthField(
+                          controller: nameController,
+                          hintText: 'Name',
+                          obscureText: false,
+                          keyboardType: TextInputType.name,
+                          prefixIcon: const Icon(Icons.person),
+                          errorMsg: _errorMsg,
+                          validator: (val) {
+                            if (val!.isEmpty) {
+                              return 'Please fill in this field';
+                            }
+                            return null;
+                          },
                         ),
+                        const SizedBox(height: 10.0),
+                        AuthField(
+                          controller: emailController,
+                          hintText: 'Email',
+                          obscureText: false,
+                          keyboardType: TextInputType.emailAddress,
+                          prefixIcon: const Icon(Icons.mail),
+                          errorMsg: _errorMsg,
+                          validator: (val) {
+                            if (val!.isEmpty) {
+                              return 'Please fill in this field';
+                            } else if (!RegExp(
+                                    r'^[\w-\.]+@([\w-]+.)+[\w-]{2,4}$')
+                                .hasMatch(val)) {
+                              return 'Please enter a valid email';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 10.0),
+                        AuthField(
+                          controller: phoneNumberController,
+                          hintText: 'Phone Number',
+                          obscureText: false,
+                          keyboardType: TextInputType.number,
+                          prefixIcon: const Icon(Icons.phone),
+                          errorMsg: _errorMsg,
+                          validator: (val) {
+                            if (val!.isEmpty) {
+                              return 'Please fill in this field';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 10.0),
+                        AuthField(
+                            controller: passwordController,
+                            hintText: 'Password',
+                            obscureText: obscurePassword,
+                            keyboardType: TextInputType.visiblePassword,
+                            prefixIcon: const Icon(Icons.password),
+                            errorMsg: _errorMsg,
+                            validator: (val) {
+                              if (val!.isEmpty) {
+                                return 'Please fill in this field';
+                              } else if (!RegExp(
+                                      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~`)\%\-(_+=;:,.<>/?"[{\]}\|^]).{8,}$')
+                                  .hasMatch(val)) {
+                                return 'Please enter a valid password';
+                              }
+                              return null;
+                            },
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  obscurePassword = !obscurePassword;
+                                });
+                              },
+                              icon: Icon(
+                                obscurePassword
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                                color: Theme.of(context).colorScheme.primary,
+                              ),
+                            )),
+                        const SizedBox(height: 10.0),
+                        AuthField(
+                            controller: repasswordController,
+                            hintText: 'Re-Enter Password',
+                            obscureText: obscurePassword,
+                            keyboardType: TextInputType.visiblePassword,
+                            prefixIcon: const Icon(Icons.password),
+                            errorMsg: _errorMsg,
+                            validator: (val) {
+                              if (val!.isEmpty) {
+                                return 'Please fill in this field';
+                              } else if (!RegExp(
+                                      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~`)\%\-(_+=;:,.<>/?"[{\]}\|^]).{8,}$')
+                                  .hasMatch(val)) {
+                                return 'Please re-enter a confirm password';
+                              }
+                              return null;
+                            },
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  obscurePassword = !obscurePassword;
+                                });
+                              },
+                              icon: Icon(
+                                obscurePassword
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
+                                color: Theme.of(context).colorScheme.primary,
+                              ),
+                            )),
+                        const SizedBox(height: 10.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: Row(
+                                children: [
+                                  Checkbox(
+                                    value: false,
+                                    onChanged: (bool? value) {},
+                                  ),
+                                  const SizedBox(width: 8.0),
+                                  const Flexible(
+                                    child: Text(
+                                      'I have read and agree to the terms of service and privacy policy',
+                                      overflow: TextOverflow.visible,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20.0),
+                        !signUpRequired
+                            ? ElevatedButton(
+                                onPressed: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    MyUser myUser = MyUser.empty;
+                                    myUser.email = emailController.text;
+                                    myUser.name = nameController.text;
+                                    setState(() {
+                                      context.read<SignUpBloc>().add(
+                                          SignUpRequested(emailController.text,
+                                              passwordController.text));
+                                    });
+                                  }
+                                },
+                                style: ElevatedButton.styleFrom(
+                                    minimumSize:
+                                        const Size(double.infinity, 50),
+                                    backgroundColor: Theme.of(context)
+                                        .colorScheme
+                                        .onSurface),
+                                child: Text(
+                                  'Register',
+                                  style: TextStyle(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .surface),
+                                ),
+                              )
+                            : const CircularProgressIndicator(),
+                        const SizedBox(height: 40.0),
                       ],
                     ),
-                    const SizedBox(height: 20.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        context.go('/auth/login');
-                      },
-                      style: ElevatedButton.styleFrom(
-                          minimumSize: const Size(double.infinity, 50),
-                          backgroundColor:
-                              Theme.of(context).colorScheme.onSurface),
-                      child: Text(
-                        'Register',
-                        style: TextStyle(
-                            color: Theme.of(context).colorScheme.surface),
-                      ),
-                    ),
-                    const SizedBox(height: 40.0),
-                  ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-        bottomNavigationBar: AuthBottom(
-            text: "Already have an account?",
-            route: '/auth/login',
-            bgColor: Theme.of(context).colorScheme.surface,
-            color: Theme.of(context).colorScheme.onSurface));
+          bottomNavigationBar: AuthBottom(
+              text: "Already have an account?",
+              route: '/auth/login',
+              bgColor: Theme.of(context).colorScheme.surface,
+              color: Theme.of(context).colorScheme.onSurface)),
+    );
   }
 }
